f4c47816ff03304f62ad8ceda9b36b2bf9d4b225
Lucas Virolli <lucas.virolli2@gmail.com>
2022-01-10 23:06:37 -0300

    Adding whole project

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+// 705.484.450-52 070.987.720-03
+class validateCPF {
+  constructor(cpfSent) {
+    Object.defineProperty(this, 'cpfClean', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfSent.replace(/\D+/g, '')
+    });
+  }
+
+  isSequence() {
+    return this.cpfClean.charAt(0).repeat(11) === this.cpfClean;
+  }
+
+  generateNewCPF() {
+    const cpfWithoutDigits = this.cpfClean.slice(0, -2);
+    const firstDigit = validateCPF.generateDigit(cpfWithoutDigits);
+    const secondDigit = validateCPF.generateDigit(cpfWithoutDigits + firstDigit);
+    this.newCPF = cpfWithoutDigits + firstDigit + secondDigit;
+  }
+
+  static generateDigit(cpfWithoutDigits) {
+    let total = 0;
+    let reverse = cpfWithoutDigits.length + 1;
+
+    for(let numericString of cpfWithoutDigits) {
+      total += reverse * Number(numericString);
+      reverse--;
+    }
+
+    const digit = 11 - (total % 11);
+    return digit <= 9 ? String(digit) : '0';
+  }
+
+  validate() {
+    // console.log(this.cpfClean)
+    if(!this.cpfClean) return false;
+    if(typeof this.cpfClean !== 'string') return false;
+    if(this.cpfClean.length !== 11) return false;
+    if(this.isSequence()) return false;
+    this.generateNewCPF();
+
+    return this.newCPF === this.cpfClean;
+  }
+}
+
+/*
+=========CONTROLLER==========
+*/
+
+const validateButton = document.querySelector('.submit');
+const cpfSent = document.querySelector('.cpf');
+const result = document.querySelector('.screen');
+
+validateButton.addEventListener('click', () => {
+  let newValidateCPF = new validateCPF(cpfSent.value);
+  if(newValidateCPF.validate()){
+    result.value = 'CPF VÁLIDO'
+  }else {
+    result.value = 'CPF INVÁLIDO'
+  }
+});
+
+
+
